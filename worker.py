307cd21a4494844e2f077de276e115ba34685f0b
bradybellini <bbellini@pm.me>
2020-06-02 23:12:19 -0700

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,13 +1,12 @@
 import os
 import redis
-from secret import redis_pass, redis_host
+from secret import redis_pass, redis_host, redis_port
 from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
-password = config(redis_pass)
-conn = redis.from_url(config(redis_host))
+redis_connection = Redis(host=redis_host, port=redis_port, password=redis_pass)
 
 if __name__ == '__main__':
-    with Connection(conn):
+    with Connection(redis_connection):
         worker = Worker(map(Queue, listen))
         worker.work()
