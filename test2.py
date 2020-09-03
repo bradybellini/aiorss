0587f1e2b1daa87745de3f79be1f2c13d1ef3fce
bradybellini <bbellini@pm.me>
2020-09-02 21:10:50 -0700

    testing

--- test2.py
+++ test2.py
@@ -1,5 +1,15 @@
-from aiorss import rssparser
+from aiorss._hook import Hook
+import asyncio
 
-def test(feed):
-    f = rssparser(feed)
-    return f.entries
+async def send():
+    newhook = Hook('https://discordapp.com/api/webhooks/750917265624399892/3SLmaVtWyloXerm9QqMucFnT6GaOrbq00aOjR6mttLookZfRByh_r-EgR2uWgMSzN5Wg')
+    await newhook.warning('A loop has failed or something')
+    await newhook.emergency('some shit has gone down')
+
+    
+    
+    
+if __name__ == '__main__':
+    current_loops = asyncio.gather(send())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(current_loops)
